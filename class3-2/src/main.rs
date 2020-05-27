1e7d25abb14cb3705cf794ca35b05a84b9d8a288
timeday1102 <982192201@qq.com>
2020-05-27 21:34:57 +0800

    1

--- class3-2/src/main.rs
+++ class3-2/src/main.rs
@@ -23,9 +23,27 @@ fn main() {
     // 取余
     let remainder = 45 % 5;
 
+    println!("{}, {}!", sum + product + remainder, difference  + quotient );
+
     let c = 'z';
     let z = 'Z';
     let heart_eyed_cat = '😻';
-    println!("{}!", heart_eyed_cat);
-    
+    println!("{}! {}! {}!", c, z, heart_eyed_cat);
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of y is {}!", y + x as f64 + z as f64);
+    let five_hundred = tup.0;
+    let six_point_four = tup.1;
+    let one = tup.2;
+    println!("{}, {}, {}", five_hundred , six_point_four , one);
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("{}!", a[1]);
+
+    let a = [3; 5];
+    println!("{}!", a[3]);
+
+    let index = 4;
+    println!("{}!", a[index]);
 }
---
+++ class3-3/target/rls/debug/deps/class3_3-d1083f1bb9c0d2d9.d
@@ -0,0 +1,5 @@
+d:\Workspace\Rust\class3-3\target\rls\debug\deps\class3_3-d1083f1bb9c0d2d9.rmeta: src\main.rs
+
+d:\Workspace\Rust\class3-3\target\rls\debug\deps\class3_3-d1083f1bb9c0d2d9.d: src\main.rs
+
+src\main.rs:
